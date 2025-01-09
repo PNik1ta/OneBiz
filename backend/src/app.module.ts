9efc23efc7affc14f82941021c7d4696dd49a09a
PNik1ta <nikitap0zd3y3v@gmail.com>
2025-01-09 22:06:18 +0300

    Add docker-compose file and base setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,58 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { ElasticsearchLoggerService } from './logger.service';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  //const loggerService = app.get(ElasticsearchLoggerService);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://www.localhost:3000',
+      'https://www.cut-price.ge/',
+      'http://www.cut-price.ge/',
+      'https://cut-price.ge/',
+      'http://cut-price.ge/'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Ambition marketing')
+    .setDescription('Ambition marketing API description')
+    .setVersion('1.0')
+    .addTag('ambition')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // app.use((req: Request, res: Response, next) => {
+  //   express.json()(req, res, () => {
+  //     if (req.url !== '/metrics') {
+  //       loggerService.logHTTPRequest(req.method, req.url, JSON.stringify(req.body), res.statusCode);
+  //     }
+  //     next();
+  //   })
+  // });
+
+  await app.listen(5000);
 }
 bootstrap();
