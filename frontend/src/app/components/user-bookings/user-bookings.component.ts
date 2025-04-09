889082c7219aded3cf5a01ef6bfc15ca60650373
PNik1ta <nikitap0zd3y3v@gmail.com>
2025-04-09 19:24:50 +0300

    Add user bookings

--- frontend/src/app/components/user-bookings/user-bookings.component.ts
+++ frontend/src/app/components/user-bookings/user-bookings.component.ts
@@ -1,12 +1,97 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IUser } from '../../core/interfaces/user.interface';
+import { IBooking } from '../../core/interfaces/booking.interface';
+import { BookingService } from '../../core/services/booking.service';
+import { IUpdateBookingDto } from '../../core/dto/booking/update-booking.dto';
+import { EBookingStatuses } from '../../core/enums/booking-statuses.enum';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-bookings',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './user-bookings.component.html',
   styleUrl: './user-bookings.component.scss'
 })
-export class UserBookingsComponent {
+export class UserBookingsComponent implements OnInit {
   @Input() user: IUser | null = null;
+  bookings: IBooking[] = [];
+  filteredBookings: IBooking[] = [];
+
+  statusFilter: string = 'ALL';
+  dateFrom: string = '';
+  dateTo: string = '';
+
+  constructor(private readonly bookingService: BookingService) { }
+
+  ngOnInit(): void {
+    this.getBookings();
+  }
+
+  getBookings() {
+    this.bookingService.getUserBookings().subscribe((res) => {
+      const now = new Date();
+
+      this.bookings = (res.data ?? [])
+        //.filter(booking => new Date(booking.datetime) > now)
+        .sort((a, b) => new Date(a.datetime).getTime() - new Date(b.datetime).getTime());
+
+      this.applyFilters();
+    });
+  }
+
+  resetFilters(): void {
+    this.statusFilter = 'ALL';
+    this.dateFrom = '';
+    this.dateTo = '';
+    this.applyFilters();
+  }
+
+
+  applyFilters(): void {
+    const from = this.dateFrom ? new Date(this.dateFrom) : null;
+    const to = this.dateTo ? new Date(this.dateTo) : null;
+
+    if (to) {
+      to.setHours(23, 59, 59, 999);
+    }
+
+    this.filteredBookings = this.bookings.filter(booking => {
+      const date = new Date(booking.datetime);
+
+      const matchesStatus =
+        this.statusFilter === 'ALL' || booking.status === this.statusFilter;
+
+      const matchesDateFrom = !from || date >= from;
+      const matchesDateTo = !to || date <= to;
+
+      return matchesStatus && matchesDateFrom && matchesDateTo;
+    });
+  }
+
+  cancelBooking(booking: IBooking) {
+    const dto: IUpdateBookingDto = {
+      datetime: booking.datetime,
+      service_id: booking.service_id,
+      description: booking.description,
+      status: EBookingStatuses.CANCELED,
+      amount: booking.amount,
+    }
+    this.bookingService.updateBooking(dto, booking.id!).subscribe(() => {
+      this.getBookings();
+    })
+  }
 }
--- frontend/src/app/core/interfaces/booking.interface.ts
+++ frontend/src/app/core/interfaces/booking.interface.ts
@@ -2,11 +2,17 @@ import { EBookingStatuses } from "../enums/booking-statuses.enum";
 
 export interface IBooking {
   id?: number;
-  datetime: Date;
+  datetime: string;
   user_id: number;
   business_id: number;
   service_id: number;
   description: string;
   status: EBookingStatuses;
   amount?: number;
+  company_name?: string;
+  service?: string;
+  service_amount?: string;
+  service_discount?: string;
+  username?: string;
+  user_avatar_url?: string;
 }
