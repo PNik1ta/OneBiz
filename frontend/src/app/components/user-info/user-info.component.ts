b641e16b3ee33920192b98f27cac9c46b8b588c2
PNik1ta <nikitap0zd3y3v@gmail.com>
2025-04-09 20:58:36 +0300

    Add user info updating

--- frontend/src/app/components/user-info/user-info.component.ts
+++ frontend/src/app/components/user-info/user-info.component.ts
@@ -2,14 +2,74 @@ import { Component, Input } from '@angular/core';
 import { IUser } from '../../core/interfaces/user.interface';
 import { CommonModule } from '@angular/common';
 import { API_IMG_URL } from '../../core/constants/api-url';
+import { FileService } from '../../core/services/file.service';
+import { UsersService } from '../../core/services/users.service';
+import { IUpdateUserDto } from '../../core/dto/user/update-user.dto';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-info',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, FormsModule, MatInputModule, MatButtonModule],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss'
 })
 export class UserInfoComponent {
   @Input() user: IUser | null = null;
   API_IMG_URL = API_IMG_URL;
+
+  isEditingUsername = false;
+  updatedUsername: string = '';
+
+  constructor(
+    private userService: UsersService,
+    private fileService: FileService
+  ) {}
+
+  onAvatarClick(): void {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+
+    input.onchange = () => {
+      const file = input.files?.[0];
+      if (!file) return;
+
+      this.fileService.uploadFile(file).subscribe(res => {
+        const url = res[0].url;
+
+        const dto: IUpdateUserDto = {
+          avatar_url: url,
+        };
+
+        this.userService.updateUser(dto).subscribe(() => {
+          if (this.user) {
+            this.user.avatar_url = url;
+          }
+        });
+      });
+    };
+
+    input.click();
+  }
+
+  startEditingUsername(): void {
+    this.updatedUsername = this.user?.username || '';
+    this.isEditingUsername = true;
+  }
+
+  saveUsername(): void {
+    const dto: IUpdateUserDto = {
+      username: this.updatedUsername,
+    };
+
+    this.userService.updateUser(dto).subscribe(() => {
+      if (this.user) {
+        this.user.username = this.updatedUsername;
+      }
+      this.isEditingUsername = false;
+    });
+  }
 }
