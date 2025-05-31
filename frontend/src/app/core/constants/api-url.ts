c3cd56f2d1fca21f6e6991fb7dae3af6800459b4
PNik1ta <nikitap0zd3y3v@gmail.com>
2025-05-31 16:31:18 +0300

    Change api url

--- frontend/src/app/core/constants/api-url.ts
+++ frontend/src/app/core/constants/api-url.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5000/api'
-export const API_IMG_URL = 'http://localhost:5000/static'
+export const API_URL = 'http://one-biz.ru/api'
+export const API_IMG_URL = 'http://one-biz.ru/static'
